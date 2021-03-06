09c19d9cd1c35b058228005a3cd4c0bcd824e609
Sebastian <smontero@mightywizards.com>
2021-03-05 18:10:25 -0600

    Developed code in setup script to create the vrf db and user

--- db/setup.sh
+++ db/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE vrf;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vrf < /vrf-db/vrf-db.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE vrf LOGIN  password '$VRF_PASSWORD'"
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE vrf;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vrf <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE vrf to vrf;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to vrf;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to vrf;
+    GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public to vrf;
 EOSQL
-
-psql vrf < /vrf-db/vrf-db.sql
