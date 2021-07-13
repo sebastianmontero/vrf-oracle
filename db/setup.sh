03a6f195a250ff181a60251cc584dba99d29584e
Sebastian <smontero@mightywizards.com>
2021-07-13 17:50:17 -0500

    Updated db setup script

--- db/setup.sh
+++ db/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE vrf;"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vrf < /vrf-db/vrf-db.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE vrf LOGIN  password '$VRF_PASSWORD'"
+if ! psql -lqt | cut -d \| -f 1 | grep -qw vrf; then
+    echo "Database vrf does not exist, creating..."
+
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE vrf;"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vrf < /vrf-db/vrf-db.sql
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE vrf LOGIN  password '$VRF_PASSWORD'"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vrf <<-EOSQL
     GRANT ALL PRIVILEGES ON DATABASE vrf to vrf;
@@ -10,3 +13,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vrf <<-EOSQL
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to vrf;
     GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public to vrf;
 EOSQL
+
+else
+    echo "Database vrf already exists doing nothing."
+fi
